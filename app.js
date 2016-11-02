6fbe533c45af31a9df4d27f35d074eecf06052ee
creativegeniusod <creativegenius2909@gmail.com>
2016-11-02 16:00:44 +0530

    add api methods

--- app.js
+++ app.js
@@ -13,9 +13,8 @@ app.use(logger('dev'))
 /*********** checkTokenAccess ********/
 app.get('/check_token_access', function (req, res) {		
 	var username = 'ratinder'
-	var rules = { TOKENLY: 1, LTBCOIN: 100000, stackop_1:'OR' }
-	var oauth_token = 'UwtfjTkU1CFQHGYS6KMdqdhvoPIG9YEI89eyO1wV'
-	tokenapiModule.checkTokenAccess(username,rules,oauth_token).then(function(result){
+	var data = {oauth_token:'G9nIzjvKzamo3JyymepA44xjz7cSOBExILsCzvyQ', TOKENLY: 1, LTBCOIN: 100000, stackop_1:'OR' }
+	tokenapiModule.checkTokenAccess(username,data).then(function(result){
 		console.log(result);
 		res.end(JSON.stringify(result))
 	},function(err){
@@ -23,13 +22,91 @@ app.get('/check_token_access', function (req, res) {
 		res.end(JSON.stringify(err))
 	})
 })
+
+/*********** checkAddressTokenAccess ********/
+app.get('/check_address_token_access', function (req, res) {		
+	var address = '14eRVGNPQChSmSmNLH6RPjdwsNPc7rH2Z7'
+	var data = {TOKENLY: 1, LTBCOIN: 100000, stackop_1:'OR' }
+	tokenapiModule.checkAddressTokenAccess(address,data).then(function(result){
+		console.log(result);
+		res.end(JSON.stringify(result))
+	},function(err){
+		console.error(err);
+		res.end(JSON.stringify(err))
+	})
+})
+
+
+/*********** getOAuthUserFromAccessToken ********/
+app.get('/get_oauth_user_from_access_token', function (req, res) {		
+	var data = {access_token:'G9nIzjvKzamo3JyymepA44xjz7cSOBExILsCzvyQ'}
+	tokenapiModule.getOAuthUserFromAccessToken(data).then(function(result){
+		console.log(result);
+		res.end(JSON.stringify(result))
+	},function(err){
+		console.error(err);
+		res.end(JSON.stringify(err))
+	})
+})
+
+
+
+/*********** updateAccount ********/
+app.get('/update_account', function (req, res) {		
+	var data = {user_id:'94e43c25-8f80-4287-9317-34d9093cfb23',token:'G9nIzjvKzamo3JyymepA44xjz7cSOBExILsCzvyQ',current_password:'ratinder6785'}
+	tokenapiModule.updateAccount(data).then(function(result){
+		console.log(result);
+		res.end(JSON.stringify(result))
+	},function(err){
+		console.error(err);
+		res.end(JSON.stringify(err))
+	})
+})
+
 
 /*********** getPublicAddresses ********/
 app.get('/get_public_addresses', function (req, res) {		
 	var username = 'ratinder'
-	var rules = { public: 0}
-	var oauth_token = 'c8OtXdVIALo9Z0a3C8P5oiBLUxVV3w2wfFnR8EFC'
-	tokenapiModule.getPublicAddresses(username,rules,oauth_token).then(function(result){
+	var data = {}
+	tokenapiModule.getPublicAddresses(username,data).then(function(result){
+		console.log(result);
+		res.end(JSON.stringify(result))
+	},function(err){
+		console.error(err);
+		res.end(JSON.stringify(err))
+	})
+})
+
+/*********** getPublicAddressDetails ********/
+app.get('/get_public_address_details', function (req, res) {		
+	var username = 'ratinder'
+	var address = '14eRVGNPQChSmSmNLH6RPjdwsNPc7rH2Z7'
+	var data = {}
+	tokenapiModule.getPublicAddressDetails(username,address,data).then(function(result){
+		console.log(result);
+		res.end(JSON.stringify(result))
+	},function(err){
+		console.error(err);
+		res.end(JSON.stringify(err))
+	})
+})
+
+/*********** registerAddress ********/
+app.get('/register_address', function (req, res) {		
+	var postData = {address:'14eRVGNPQChSmSmNLH6RPjdwsNPc7rH2Z7', label:"Test address2", public: true, active:true, oauth_token: 'G9nIzjvKzamo3JyymepA44xjz7cSOBExILsCzvyQ', scope:'manage-address' }
+	tokenapiModule.registerAddress(postData).then(function(result){
+		console.log(result);
+		res.end(JSON.stringify(result))
+	},function(err){
+		console.error(err);
+		res.end(JSON.stringify(err))
+	})
+})
+
+/*********** getAddressesForAuthenticatedUser ********/
+app.get('/get_addresses_for_authenticated_user', function (req, res) {	
+	var data = {oauth_token:'G9nIzjvKzamo3JyymepA44xjz7cSOBExILsCzvyQ',scope:'tca,manage-address,private-address'}
+	tokenapiModule.getAddressesForAuthenticatedUser(data).then(function(result){
 		console.log(result);
 		res.end(JSON.stringify(result))
 	},function(err){
@@ -39,11 +116,104 @@ app.get('/get_public_addresses', function (req, res) {
 })
 
 
-/*********** registerAddress ********/
-app.get('/register_address', function (req, res) {		
-	var postData = {address:'Mohali Punjab', label:"Test My address", public: true, active:true }
-	var oauth_token = 'UwtfjTkU1CFQHGYS6KMdqdhvoPIG9YEI89eyO1wV'
-	tokenapiModule.registerAddress(postData,oauth_token).then(function(result){
+/*********** getAddressDetailsForAuthenticatedUser ********/
+app.get('/get_address_details_for_authenticated_user', function (req, res) {
+	var address = '14eRVGNPQChSmSmNLH6RPjdwsNPc7rH2Z7'	
+	var data = {oauth_token:'G9nIzjvKzamo3JyymepA44xjz7cSOBExILsCzvyQ',scope:'tca,manage-address,private-address'}
+	tokenapiModule.getAddressDetailsForAuthenticatedUser(data,address).then(function(result){
+		console.log(result);
+		res.end(JSON.stringify(result))
+	},function(err){
+		console.error(err);
+		res.end(JSON.stringify(err))
+	})
+})
+
+/*********** verifyAddress ********/
+app.get('/verify_address', function (req, res) {
+	var address = '14eRVGNPQChSmSmNLH6RPjdwsNPc7rH2Z7'	
+	var data = {oauth_token:'G9nIzjvKzamo3JyymepA44xjz7cSOBExILsCzvyQ',scope:'tca,manage-address,private-address'}
+	tokenapiModule.verifyAddress(data,address).then(function(result){
+		console.log(result);
+		res.end(JSON.stringify(result))
+	},function(err){
+		console.error(err);
+		res.end(JSON.stringify(err))
+	})
+})
+
+
+/*********** updateAddressDetails ********/
+app.get('/update_address_details', function (req, res) {
+	var address = '14eRVGNPQChSmSmNLH6RPjdwsNPc7rH2Z7'	
+	var data = {label:'Test update address',public: true, active:true,oauth_token:'G9nIzjvKzamo3JyymepA44xjz7cSOBExILsCzvyQ',scope:'tca,manage-address,private-address'}
+	tokenapiModule.updateAddressDetails(data,address).then(function(result){
+		console.log(result);
+		res.end(JSON.stringify(result))
+	},function(err){
+		console.error(err);
+		res.end(JSON.stringify(err))
+	})
+})
+
+
+/*********** deleteAddress ********/
+app.get('/delete_address', function (req, res) {
+	var address = '1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX'	
+	var data = {oauth_token:'G9nIzjvKzamo3JyymepA44xjz7cSOBExILsCzvyQ',scope:'tca,manage-address,private-address'}
+	tokenapiModule.deleteAddress(data,address).then(function(result){
+		console.log(result);
+		res.end(JSON.stringify(result))
+	},function(err){
+		console.error(err);
+		res.end(JSON.stringify(err))
+	})
+})
+
+
+/*********** lookupUserByAddress ********/
+app.get('/lookup_user_by_address', function (req, res) {
+	var address = '14eRVGNPQChSmSmNLH6RPjdwsNPc7rH2Z7'
+	var data = {}
+	tokenapiModule.lookupUserByAddress(data,address).then(function(result){
+		console.log(result);
+		res.end(JSON.stringify(result))
+	},function(err){
+		console.error(err);
+		res.end(JSON.stringify(err))
+	})
+})
+
+
+/*********** lookupAddressByUser ********/
+app.get('/lookup_address_by_user', function (req, res) {
+	var username = 'ratinder'
+	var data = {}
+	tokenapiModule.lookupAddressByUser(data,username).then(function(result){
+		console.log(result);
+		res.end(JSON.stringify(result))
+	},function(err){
+		console.error(err);
+		res.end(JSON.stringify(err))
+	})
+})
+
+/*********** getCombinedPublicBalances ********/
+app.get('/get_combined_public_balances', function (req, res) {
+	var data = {oauth_token:'G9nIzjvKzamo3JyymepA44xjz7cSOBExILsCzvyQ',scope:'tca,manage-address,private-address'}
+	tokenapiModule.getCombinedPublicBalances(data).then(function(result){
+		console.log(result);
+		res.end(JSON.stringify(result))
+	},function(err){
+		console.error(err);
+		res.end(JSON.stringify(err))
+	})
+})
+
+/*********** getCombinedProtectedBalances ********/
+app.get('/get_combined_protected_balances', function (req, res) {
+	var data = {oauth_token:'G9nIzjvKzamo3JyymepA44xjz7cSOBExILsCzvyQ',scope:'tca,manage-address,private-address'}
+	tokenapiModule.getCombinedProtectedBalances(data).then(function(result){
 		console.log(result);
 		res.end(JSON.stringify(result))
 	},function(err){
